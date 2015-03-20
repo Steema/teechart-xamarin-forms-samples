e39a394a7f5c6ca2ac7e03aaf6f64882f16bc652
Narcís Calvet <ncalvet@gmail.com>
2015-03-20 16:32:01 +0100

    Unified demo

---
+++ TeeChartDemo/TeeChartDemo.PCL/TeeChartDemo.PCL.iOS/TeeChartDemo.PCL.iOS.Unified/ChartViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TeeChartDemo.PCL.ChartView), typeof(TeeChartDemo.PCL.iOS.ChartViewRenderer))]
+namespace TeeChartDemo.PCL.iOS
+{
+     //UIView, IViewRenderer, IDisposable, IRegisterable
+  public class ChartViewRenderer : ViewRenderer<ChartView, Steema.TeeChart.TChart>
+  {      
+    protected override void OnElementChanged(ElementChangedEventArgs<ChartView> e)
+    {
+      base.OnElementChanged(e);
+      if (e.OldElement != null || this.Element == null)
+        return;
+
+      var chartView = new Steema.TeeChart.TChart(); 
+
+      chartView.Chart = Element.Model;
+
+      SetNativeControl(chartView);
+    }
+
+    protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      base.OnElementPropertyChanged(sender, e);
+    }
+  }
+}
