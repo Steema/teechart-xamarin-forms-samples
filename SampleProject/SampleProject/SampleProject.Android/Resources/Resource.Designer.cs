e5e0e3c79c0ed20439e67dfc9526a1ba04e2e015
Narcís Calvet <narcis@steema.com>
2016-06-17 16:07:18 +0200

    Xamarin.Forms 2.3.0.49 upgrade

--- SampleProject/SampleProject/SampleProject.Android/Resources/Resource.Designer.cs
+++ SampleProject/SampleProject/SampleProject.Android/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace SampleProject.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::SampleProject.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
