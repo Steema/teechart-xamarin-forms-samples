0003b4b5814d701bcee00e387735667e0bf02994
rubenglam <rubenbermudezsaez@gmail.com>
2019-07-05 10:45:18 +0200

    Minor change

--- ChartGestures/ChartGestures/MainPage.xaml.cs
+++ ChartGestures/ChartGestures/MainPage.xaml.cs
@@ -9,8 +9,6 @@ using Xamarin.Forms;
 
 namespace ChartGestures
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
